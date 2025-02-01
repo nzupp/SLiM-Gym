6ffad941caed1d40e6459978a4ca6d6fbbe19062
nzupp <nzuppas6@gmail.com>
2025-02-01 12:42:32 -0500

    Fixed init

--- slim_gym/__init__.py
+++ slim_gym/__init__.py
@@ -11,3 +11,10 @@ from .slim_gym import SLiMGym
 from .sfs import SFSGym
 from .slim_injector import create_slim_script
 from .random_walk import run_random_agent
+
+__all__ = [
+    'SLiMGym',
+    'SFSGym',
+    'create_slim_script',
+    'run_random_agent'
+]
