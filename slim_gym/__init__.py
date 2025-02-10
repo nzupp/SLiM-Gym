c5acd54326e80ee5dd324ab8d33a123ecb34741f
nzupp <nzuppas6@gmail.com>
2025-02-10 15:10:34 -0500

    Fixed imports

--- slim_gym/__init__.py
+++ slim_gym/__init__.py
@@ -5,13 +5,12 @@
 SLiM-Gym: A gymnasium environment for SLiM evolutionary simulations
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .slim_gym_wrapper import SLiMGym
 from .sfs import SFSGym
 from .slim_injector import create_slim_script
 from .examples.make_env import make_env
-from .examples.SB3_PPO import train_agent
 from .examples.random_walk import run_random_agent
 
 __all__ = [
