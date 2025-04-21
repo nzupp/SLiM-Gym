423b7279ed7e8189dac91774aedd7eb84f46f9e0
Niko Zuppas <nzuppas6@gmail.com>
2025-04-21 12:08:58 -0400

    Update __init__.py

--- slim_gym/__init__.py
+++ slim_gym/__init__.py
@@ -9,14 +9,14 @@ __version__ = "0.2.7"
 from .envs.base_env import SLiMGym
 from .envs.sfs_env import SFSGym
 from .envs.make_sfs_env import make_sfs_env
-
-# Import utilities
 from .utils.utils import check_slim_installed, validate_slim_script
+from .examples.SB3_PPO_bottleneck import PPO_example
 
 __all__ = [
     'SLiMGym',
     'SFSGym',
     'make_sfs_env',
     'check_slim_installed',
-    'validate_slim_script'
+    'validate_slim_script',
+    'PPO_example'
 ]
