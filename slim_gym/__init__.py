0006a8fe7cfc2950a4bf63660c4a097d12a678e2
Niko Zuppas <nzuppas6@gmail.com>
2025-04-21 10:49:18 -0400

    Update __init__.py

--- slim_gym/__init__.py
+++ slim_gym/__init__.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 """
 @author: nzupp
-
 SLiM-Gym: A gymnasium environment for SLiM evolutionary simulations
 """
-
 __version__ = "0.2.7"
 
-from .slim_gym_wrapper import SLiMGym
-from .sfs import SFSGym
-from .slim_injector import create_slim_script
-from .examples.make_env import make_env
-from .examples.random_walk import run_random_agent
+# Import core components
+from .envs.base_env import SLiMGym
+from .envs.sfs_env import SFSGym
+from .envs.make_sfs_env import make_sfs_env
+
+# Import utilities
+from .utils.utils import check_slim_installed, validate_slim_script
 
 __all__ = [
     'SLiMGym',
     'SFSGym',
-    'create_slim_script',
-    'run_random_agent',
-    'make_env',
-    'train_agent'
+    'make_sfs_env',
+    'check_slim_installed',
+    'validate_slim_script'
 ]
