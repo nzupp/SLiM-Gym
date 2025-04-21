faf80f7bc6a105ffbd53948274eff28803041a21
Niko Zuppas <niko@Nikos-MacBook-Air.local>
2025-04-21 13:14:12 -0400

    v0.2.12

--- slim_gym/__init__.py
+++ slim_gym/__init__.py
@@ -3,7 +3,7 @@
 @author: nzupp
 SLiM-Gym: A gymnasium environment for SLiM evolutionary simulations
 """
-__version__ = "0.2.16"
+__version__ = "0.2.12"
 
 # Import core components
 from .envs.base_env import SLiMGym
