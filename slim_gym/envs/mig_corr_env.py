101cea5f9e97c600ffb757de4a66217f6fd08210
Niko Zuppas <nzuppas6@gmail.com>
2025-05-06 15:58:23 -0400

    Update mig_corr_env.py

--- slim_gym/envs/mig_corr_env.py
+++ slim_gym/envs/mig_corr_env.py
@@ -1,1 +1,14 @@
+"""
+@author: nzupp
 
+Sample environment to test migratory corridor placement (think land bridges over highways)
+
+Extends the main four key functions created by the base env
+1) Process initial state
+2) Process state
+3) Process action
+4) Calculate reward
+
+The action taken each step will represent one connection placed on a spatial grid. The 
+observation will be based on genetic diversity and inbreeding
+"""
